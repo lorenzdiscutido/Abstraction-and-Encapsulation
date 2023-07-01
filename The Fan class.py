319425acd87eb7f7c59b5c3cd658083a89ee8a79
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-01 20:45:07 +0800

    Created a constructor with the fan's default features and the variables for speed, power, radius, and color

--- The Fan class.py
+++ The Fan class.py
@@ -5,10 +5,14 @@ class Fan:
     medium = 2
     fast = 3
     #Create a constructor for speed, radius, color, and power
-    def __init__(self, speed, radius, color, on=False)
+    def __init__(self, speed = slow, radius = 5, color = "blue", on=False):
     #Private field for speed of the fan
+        self.__speed = speed
     #Private field if the fan is on or off
+        self.__power = on
     #Private float field for radius of fan
+        self.__radius = radius
     #Private string field for the color of the fan
+        self.__color = color
     #getters and setters for the private data fields
 
