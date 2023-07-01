7db3722b1e48e62eb6d139ddb9e2619f8a7a7c13
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-01 14:43:31 +0800

    Added the fan speed and the constructor in getting the characteristics of the fan

--- The Fan class.py
+++ The Fan class.py
@@ -1,9 +1,14 @@
 #Create a class named Fan
+class Fan:
     #Declare three constant: slow, medium, and fast
+    slow = 1
+    medium = 2
+    fast = 3
+    #Create a constructor for speed, radius, color, and power
+    def __init__(self, speed, radius, color, on=False)
     #Private field for speed of the fan
     #Private field if the fan is on or off
     #Private float field for radius of fan
     #Private string field for the color of the fan
     #getters and setters for the private data fields
 
-#Create a constructor for speed, radius, color, and power
