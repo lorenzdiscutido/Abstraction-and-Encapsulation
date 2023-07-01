168f4f18fa31479246e14eca9fdef2eac3838a92
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-01 23:05:26 +0800

    Adjusted some parts of the code. The program is working fine and doing what is intended

--- TestFan.py
+++ TestFan.py
@@ -2,10 +2,11 @@ from The_fan_class import Fan
 
 class TestFan:
     def __init__(self):
-        self.fan1 = Fan(Fan.fast, 10, "Yellow", False)
-        self.fan2 = Fan(Fan.medium, 5, "Blue", True)
+        self.fan1 = Fan(Fan.fast, 10, "Yellow", True)
+        self.fan2 = Fan(Fan.medium, 5, "Blue", False)
 
     def show(self):
+        print("")
         print("Fan 1")
         print("Speed:", self.fan1.get_speed())
         print("Radius:", self.fan1.get_radius())
@@ -18,5 +19,6 @@ class TestFan:
         print("Radius:", self.fan2.get_radius())
         print("Color:", self.fan2.get_color())
         print("On:", self.fan2.get_power())
+        print("")
 test_run = TestFan()
 test_run.show()
