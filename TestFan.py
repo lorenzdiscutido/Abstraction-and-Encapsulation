edc2fcda11f74905e741802d4b687e58308b44ee
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-01 21:49:49 +0800

    Set the specified speed of the fan

--- TestFan.py
+++ TestFan.py
@@ -4,4 +4,10 @@ class TestFan:
     def test(self):
         fan1 = Fan()
         fan2 = Fan()
-        
+
+        #Set speed
+        fan1.set_speed(Fan.fast)
+        fan2.set_speed(Fan.medium)
+        #Set radius
+        #Set color
+        #Set power
