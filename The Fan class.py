fc33fb759725999ef7f30ff1d577f8606c7a9b34
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-01 13:22:19 +0800

    Pseudocode

---
+++ The Fan class.py
@@ -0,0 +1,9 @@
+#Create a class named Fan
+    #Declare three constant: slow, medium, and fast
+    #Private field for speed of the fan
+    #Private field if the fan is on or off
+    #Private float field for radius of fan
+    #Private string field for the color of the fan
+    #getters and setters for the private data fields
+
+#Create a constructor for speed, radius, color, and power
