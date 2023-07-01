60a9b96561f83647c7ac1dd337b3ff85950ab31d
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-01 22:19:53 +0800

    The code does not have an error message but is not printing

--- TestFan.py
+++ TestFan.py
@@ -2,12 +2,12 @@ from The_fan_class import Fan
 
 class TestFan:
     def __init__(self):
-        fan1 = Fan(Fan.fast, 10, "Yellow", False)
-        fan2 = Fan(Fan.medium, 5, "Blue", True)
+        self.fan1 = Fan(Fan.fast, 10, "Yellow", False)
+        self.fan2 = Fan(Fan.medium, 5, "Blue", True)
 
     def show(self):
         print("Fan 1")
-        print("Speed:", )
+        print("Speed:", self.fan1.get_speed)
 
 test_run = TestFan()
 test_run.show
