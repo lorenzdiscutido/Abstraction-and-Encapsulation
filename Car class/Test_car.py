586ec8c060a0472363e5ecddf40dc39490c1cef3
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-02 12:25:21 +0800

    Accelerate the car five times

--- Car class/Test_car.py
+++ Car class/Test_car.py
@@ -3,4 +3,9 @@ from car_class import Car
 class TestCar:
     #create the object
     car = Car(2004, "Honda")
-        
+    
+    #acceleration of the car(5x)
+    for i in range(5):
+        car.accelerate()
+        print("The car's speed is:", car.get_speed())
+    #brake of the car(5x)
