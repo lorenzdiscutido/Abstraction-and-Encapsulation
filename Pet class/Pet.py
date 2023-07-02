2d9c562df5cc5b5870f8955293cead9aa43b2579
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-02 18:36:26 +0800

    Created the class pet and the constructor

---
+++ Pet class/Pet.py
@@ -0,0 +1,13 @@
+#Make a class for pets
+class Pets:
+    def __init__(self, name, animal_type,age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+#Make a constructor for the pets
+#set name
+#set animal type
+#set age
+#get name
+#get animal type
+#get age
