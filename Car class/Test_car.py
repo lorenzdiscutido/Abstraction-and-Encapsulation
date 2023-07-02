e43e820cc3c09ecd6be2a60f3a01fb0c1b0db4a2
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-02 12:29:45 +0800

    Added the function for brake and for showing the output

--- Car class/Test_car.py
+++ Car class/Test_car.py
@@ -2,10 +2,18 @@ from car_class import Car
 #create a class
 class TestCar:
     #create the object
-    car = Car(2004, "Honda")
-    
-    #acceleration of the car(5x)
-    for i in range(5):
-        car.accelerate()
-        print("The car's speed is:", car.get_speed())
-    #brake of the car(5x)
+    def show(self):
+        car = Car(2004, "Honda")
+        
+        #acceleration of the car(5x)
+        for i in range(5):
+            car.accelerate()
+            print("The car's speed is:", car.get_speed())
+
+        #brake of the car(5x)
+        for i in range(5):
+            car.brake()
+            print("The car's speed is:", car.get_speed())
+
+test_run = TestCar
+test_run.show()
