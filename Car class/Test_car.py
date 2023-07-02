cdac705bac880c0290e35c6d7f85464b99e7497d
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-02 12:47:05 +0800

    The code is working properly and output what is intended

--- Car class/Test_car.py
+++ Car class/Test_car.py
@@ -1,14 +1,11 @@
 from car_class import Car
-#create a class
-    #create the object
+
 car = Car(2004, "Honda")
         
-#acceleration of the car(5x)
 for i in range(5):
     car.accelerate()
     print("The car's speed is:", car.get_speed())
 
-    #brake of the car(5x)
 for i in range(5):
     car.brake()
     print("The car's speed is:", car.get_speed())
--- Car class/car_class.py
+++ Car class/car_class.py
@@ -1,17 +1,15 @@
-#Make a class
 class Car:
-#create a constructor for its attributes
     def __init__(self, year_model, make):
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
-#function for year model
+
     def year_model(self):
         return self.__year_model
-#Function for make
+
     def make(self):
         return self.__make
-#function for speed
+
     def accelerate(self):
         self.__speed += 5
         
