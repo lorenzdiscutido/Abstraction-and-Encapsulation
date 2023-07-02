93dfc74f2ffc32a26d6f387edcfd5cad14602773
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-02 11:50:44 +0800

    Created a class and a constructor for car's attribute

--- Car class/car_class.py
+++ Car class/car_class.py
@@ -1,5 +1,10 @@
 #Make a class
+class Car:
 #create a constructor for its attributes
-#function for year moderl
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+#function for year model
 #Function for make
 #function for speed
