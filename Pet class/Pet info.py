ea5e0b4128325f7ba4d627dad58f6e82b6df4932
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-02 19:26:20 +0800

    The constructor was missing some parts causing the program to error but is fixed. Also added the function that prompts the user to input infos

--- Pet class/Pet info.py
+++ Pet class/Pet info.py
@@ -3,6 +3,12 @@ from Pet import Pets
 #make a new class
 class pet_infos:
     def get_info(self):
-        pet = Pets()
+        pet = Pets("", "", 0)
 #prompt the user to enter name, type, and age
+        pet.set_name()
+        pet.set_animal_type()
+        pet.set_age()
 #Display the name, type, and age
+
+pet = pet_infos()
+pet.get_info()
