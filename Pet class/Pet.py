d06dec949f5071b24f5f8f4169001cccd8354efd
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-02 18:42:01 +0800

    Added the function for setting the pet's name, type, and age

--- Pet class/Pet.py
+++ Pet class/Pet.py
@@ -1,13 +1,22 @@
 #Make a class for pets
 class Pets:
+    #Make a constructor for the pets
     def __init__(self, name, animal_type,age):
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
-#Make a constructor for the pets
-#set name
-#set animal type
-#set age
-#get name
-#get animal type
-#get age
+    #set name
+    def set_name(self):
+        name = input("Please input your pet's name:")
+        self.__name = name
+    #set animal type
+    def set_animal_type(self):
+        animal_type = input("Please specify your pet's animal type:")
+        self.__animal_type = animal_type
+    #set age
+    def set_age(self):
+        age = int(input("Please enter your pet's age:"))
+        self.__age = age
+    #get name
+    #get animal type
+    #get age
