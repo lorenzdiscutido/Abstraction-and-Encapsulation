43b6e3708711e73b162db3019223f0743a2d0803
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-07-02 19:17:07 +0800

    Imported the class from the other file then created a new class for the pet's information

--- Pet class/Pet info.py
+++ Pet class/Pet info.py
@@ -1,4 +1,8 @@
 #Import the class fromt the other file
+from Pet import Pets
 #make a new class
+class pet_infos:
+    def get_info(self):
+        pet = Pets()
 #prompt the user to enter name, type, and age
 #Display the name, type, and age
